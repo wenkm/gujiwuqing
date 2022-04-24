c33f5aee8550a608f1ed3b705b2ab466b52c68c1
Kevin <i@mkev.in>
2022-04-24 15:06:49 +0800

    更新配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,13 @@
 export default {
     SITE_NAME: '山海依无归期',
-    SITE_URL: 'http://hui.k5m.cn'
+    SITE_URL: 'http://hui.k5m.cn',
+    NICK_NAME: '估计有情',
+    EMAIL: 'i@mkev.in',
+    GITHUB: 'https://github.com/wenkm/gujiwuqing',
+    GITEE: 'https://gitee.com/fwh1998',
+    HOMETOWN: '江苏九江',
+    MOTTO: '身为蝼蚁，当有鸿鹄之志',
+    CURRENT_ADDRESS: '浙江杭州',
+    JOB: '前端工程师',
+    TITLE: '高级前端工程师',
 }
